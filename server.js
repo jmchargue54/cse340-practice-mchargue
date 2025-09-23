2cd410ebcccaa6b10c948a4ed1307e12d78f7df0
jmchargue54 <jmchargue54@users.noreply.github.com>
2025-09-23 16:34:38 -0600

    new html and css

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-// Import express using ESM syntax
+// Imports
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// Create an instance of an Express application
+// Variables
 const app = express();
 const name = process.env.NAME;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a route handler for the root URL ('/')
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-// Define the port number the server will listen on
-const PORT = 3000;
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
